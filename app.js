b3a4871ce541931f3088cf5cea6c59c9cba578fa
screwyterror83 <screwyterror83@gmail.com>
2020-04-24 02:18:44 -0700

    init base file and package.json

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000
+};
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log("Server started on port " + port)
+  }
+});
